ca9cdfb33a823fb10fe5883f3c7b036ab974e103
Willian Marchesan <willian.marchesan@gmail.com>
2024-01-26 21:58:42 -0300

    Doc: Added various Future tests

--- test/dart_ddi_test.dart
+++ test/dart_ddi_test.dart
@@ -1,14 +1,19 @@
 import 'beans_test/add_decoratos_test.dart';
+import 'beans_test/application_future_test.dart';
 import 'beans_test/application_test.dart';
 import 'beans_test/circular_injection_test.dart';
+import 'beans_test/dependent_future_test.dart';
 import 'beans_test/dependent_test.dart';
 import 'beans_test/dispose_destroy_all_session_test.dart';
 import 'beans_test/get_by_type_test.dart';
 import 'beans_test/interceptor_test.dart';
+import 'beans_test/object_future_test.dart';
 import 'beans_test/object_test.dart';
 import 'beans_test/post_construct_pre_destroy_test.dart';
 import 'beans_test/register_if_test.dart';
+import 'beans_test/session_future_test.dart';
 import 'beans_test/session_test.dart';
+import 'beans_test/singleton_future_test.dart';
 import 'beans_test/singleton_test.dart';
 import 'event_test/event_test.dart';
 import 'stream_test/stream_test.dart';
@@ -25,6 +30,13 @@ void main() {
   disposeDestroyAllSession();
   registerIf();
   postConstructPreDestroyTest();
+
+  //Futures
+  applicationFuture();
+  singletonFuture();
+  sessionFuture();
+  dependentFuture();
+  objectFuture();
 
   //Decorators
   addDecorator();
