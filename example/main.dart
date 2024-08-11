6b200c0057ee0bd689743518ef482abe7e1c9dcb
Willian Marchesan <willian.marchesan@gmail.com>
2024-08-11 18:52:36 -0300

    Doc: Format

--- example/main.dart
+++ example/main.dart
@@ -136,18 +136,21 @@ void main() async {
   myService2.doSomething();
 
   // Get an instance of MyLoggingService with qualifier
-  final MyLoggingService myLoggingSession = ddi.get(qualifier: 'MyLoggingSession');
+  final MyLoggingService myLoggingSession =
+      ddi.get(qualifier: 'MyLoggingSession');
 
   // Call a method on the MyLoggingService instance
   myLoggingSession.logSomething();
 
   // Get another instance of MyLoggingService with different qualifier
-  final MyLoggingService myLoggingDependent = ddi.get(qualifier: 'MyLoggingDependent');
+  final MyLoggingService myLoggingDependent =
+      ddi.get(qualifier: 'MyLoggingDependent');
   myLoggingDependent.logSomething();
 
   // Add a decorator to uppercase strings
   String uppercaseDecorator(String str) => str.toUpperCase();
-  ddi.registerObject('Hello World', qualifier: 'authored', decorators: [uppercaseDecorator]);
+  ddi.registerObject('Hello World',
+      qualifier: 'authored', decorators: [uppercaseDecorator]);
 
   // Will return HELLO WORLD
   print(ddi.get(qualifier: 'authored'));
